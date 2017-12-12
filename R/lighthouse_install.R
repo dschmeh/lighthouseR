5db05f291f439e72381d5d8538b369c8209a90a9
Daniel Schmeh <daniel.schmeh@atript.com>
2017-12-12 04:06:23 +0100

    Create lighthouse_install.R

---
+++ R/lighthouse_install.R
@@ -0,0 +1,10 @@
+#' Lighthouse_install Function
+#'
+#' This function allows you to install Lighthouse on your lovcal machine.
+#' lighthouse_install()
+
+
+lighthouse_install <- function() {
+  #System Call to install lighthouse on the local machine
+  system("npm install -g lighthouse")
+}
